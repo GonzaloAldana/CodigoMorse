dbc224dabe558d078c6f4fc21cf50f6e3555be9a
GonzaloAldana <30382268+GonzaloAldana@users.noreply.github.com>
2017-07-24 00:03:43 -0500

    Add files via upload

---
+++ PruebaMorse.ino
@@ -0,0 +1,28 @@
+int cont = 0;
+int buttonstate = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // Lleva un boton conectado al puerto 2
+  pinMode(2, INPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonstate = digitalRead(2);
+  if(buttonstate == HIGH)
+  {
+    // Si el botón está presionado envia el npumero de ciclos 
+    // durante los que ha estado presionado
+    cont ++;
+  }
+  else
+  {
+    // Si el boton no está presionado, envia puros ceros
+    cont = 0;
+  }  
+  Serial.println(cont);
+}
